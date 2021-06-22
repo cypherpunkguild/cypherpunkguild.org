765074ae70c7ca145e8b08ad74e319426e7d5800
andreybo <andreybobrovnik@mail.ru>
2021-06-23 00:48:23 +0300

    bio, qr, events

--- js/script.js
+++ js/script.js
@@ -19,3 +19,19 @@ $(function() {
             }
     });
 });
+
+//copy to clipboard
+spans = document.querySelectorAll(".copy");
+for (const span of spans) {
+  span.onclick = function() {
+    document.execCommand("copy");
+  }
+
+  span.addEventListener("copy", function(event) {
+    event.preventDefault();
+    if (event.clipboardData) {
+      event.clipboardData.setData("text/plain", span.textContent);
+      console.log(event.clipboardData.getData("text"))
+    }
+  });
+}
